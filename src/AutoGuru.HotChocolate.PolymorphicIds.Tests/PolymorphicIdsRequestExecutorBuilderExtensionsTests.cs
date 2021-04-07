8b3e7eba9e091f47836cc709aff84a99c14c94d4
Ben McCallum <ben.mccallum@live.com.au>
2021-04-07 12:08:39 +0200

    More test coverage

---
+++ src/AutoGuru.HotChocolate.PolymorphicIds.Tests/PolymorphicIdsRequestExecutorBuilderExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Xunit;
+
+namespace AutoGuru.HotChocolate.PolymorphicIds.Tests
+{
+    public class PolymorphicIdsRequestExecutorBuilderExtensionsTests
+    {
+        [Fact]
+        public void Should_Throw_If_Null_Builder()
+        {
+            var ex = Should.Throw<ArgumentNullException>(
+                () => PolymorphicIdsRequestExecutorBuilderExtensions.AddPolymorphicIds(null!));
+            ex.ParamName.ShouldBe("builder");
+        }
+    }
+}
